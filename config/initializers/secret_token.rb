91ac442bc9f8bebcce086af4abb0ef2cf6ab7760
jalil mohammed <jalilsan@gmail.com>
2013-10-11 16:09:19 +0900

    INITIAL commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dearfriends::Application.config.secret_key_base = '6c39e8780123b685a1bd7384799c8c304701606f6a0d6dd9ba22653d7002d14d049e12a6a973f57523de3c038af81bc84e9d017f8c05da005ce48ea723012232'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dearfriends::Application.config.session_store :cookie_store, key: '_dearfriends_session'
