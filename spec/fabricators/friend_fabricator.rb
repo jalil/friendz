61264c9846c1777d205c1def8df0b6d7413ee7e3
jalil mohammed <jalilsan@gmail.com>
2013-10-14 04:50:38 +0900

    User spec

---
+++ spec/fabricators/friend_fabricator.rb
@@ -0,0 +1,6 @@
+Fabricator(:friend) do
+  name   'Rick James'
+  email   'rick@example.com'
+  message  'This is my message'
+  user
+end
---
+++ spec/fabricators/user_fabricator.rb
@@ -0,0 +1,5 @@
+Fabricator(:user) do
+  first_name 'Bob'
+  last_name   'Hope'
+  email      'bob@example.com'
+end
