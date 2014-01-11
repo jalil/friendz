efac2cdcc2f699789d0aa04300572da3ef1b2192
Jalil <jalilsan@gmail.com>
2014-01-12 06:46:47 +0900

    WIP

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,12 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-    redirect_to @user 
+    if @user.save
+       session[:user_id] = @user.id
+       redirect_to root_url, notice: "Thank you for signing up!"
+    else
+       render 'new'
+    end
   end
 
   private
